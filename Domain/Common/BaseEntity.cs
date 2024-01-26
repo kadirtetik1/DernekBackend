d1f6f51e75f82eee65a68e78e38cf78521860e49
kadirtetik1 <tetik.kadir@hotmail.com>
2024-01-26 10:26:12 +0300

    date time oluşturuldu

--- Domain/Common/BaseEntity.cs
+++ Domain/Common/BaseEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Domain.Common
     public class BaseEntity
     {
         public Guid Id { get; set; }
-        public DateTime CreatedTime { get; set; }
+        public DateTime CreatedTime { get; set; } = DateTime.UtcNow;
+        //public DateTime CreatedTime { get; set; } = DateTime.UtcNow.ToString("yyyy-mm-dd");
+
     }
 }
